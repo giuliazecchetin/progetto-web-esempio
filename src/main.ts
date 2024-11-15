cce05824eca749e45cb7d8f086514e2b0f754a9f
zecchi <giulia.zecchetin@itsincom.it>
2024-11-15 13:00:43 +0100

    Aggiunto un bottone

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-/* eslint-disable no-console */
-
-function main(): void
-{
-    console.log("Hello, world!");
+function gcd(a: number, b: number): number {
+    while (b !== 0) {
+        const temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
 
-main();
+function lcm(a: number, b: number): number {
+    return Math.abs(a * b) / gcd(a, b);
+}
+
